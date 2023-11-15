fbd7d0723bff434b486c3d9c98cb77ee940f013b
mipseb <germtoo@outlook.com>
2023-11-15 05:09:58 -0500

    Autoconf for 1.6.3 is broken, fix and modernise - ran `autoupdate` - Dropped obsolete `STDC_HEADERS` - Fix: AC_CONFIG_SRCDIR set to configure.ac - INCLUDES -> AM_CPPFLAGS
    Signed-off-by Mipseb Germ <germtoo@outlook.com>

--- src/cronoutils.h
+++ src/cronoutils.h
@@ -93,18 +93,11 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-
-#if TIME_WITH_SYS_TIME
-# include <sys/time.h>
-# include <time.h>
+#if HAVE_SYS_TIME_H
+#include <sys/time.h>
 #else
-# if HAVE_SYS_TIME_H
-#  include <sys/time.h>
-# else
-#  include <time.h>
-# endif
+#include <time.h>
 #endif
-
 
 #ifdef _WIN32
 #define mode_t int
